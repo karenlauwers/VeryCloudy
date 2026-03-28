358b92188a1f84b6c9f6175dd765c536473093b5
KAREN_HPZ\karen <karen.lauwers1@gmail.com>
2026-03-28 20:01:10 +0100

    Remove columns 'cape' and temp at height, not in historical data of Open Meteo

--- scripts/main_weather.py
+++ scripts/main_weather.py
@@ -7,12 +7,12 @@ Usage:
 
 import asyncio
 
-from verycloudy.config import FILEPATH_CLOUDS_WITH_DATE_LOCATION_TESTSET, FILEPATH_WEATHER_TEST_V4
+from verycloudy.config import FILEPATH_CLOUDS_WITH_DATE_LOCATION_TESTSET, FILEPATH_WEATHER_TEST_V5
 from weather import run
 
 
 async def main():
-    await run(FILEPATH_CLOUDS_WITH_DATE_LOCATION_TESTSET, FILEPATH_WEATHER_TEST_V4)
+    await run(FILEPATH_CLOUDS_WITH_DATE_LOCATION_TESTSET, FILEPATH_WEATHER_TEST_V5)
 
 
 if __name__ == "__main__":
